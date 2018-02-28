ded3ae0e9d6433e70217f2935e7fca51a55b1d8e
ahad <ahad.ahmed@surecash.net>
2018-02-28 13:04:37 +0600

    First commit

---
+++ src/com/maico/algorithmpractice/tree/Node.java
@@ -0,0 +1,15 @@
+package com.maico.algorithmpractice.tree;
+
+public class Node {
+	
+	private int nodeValue;
+	private Node leftNode;
+	private Node rightNode;
+	
+	public Node(int nodeValue) {
+		this.nodeValue = nodeValue;
+		this.leftNode = this.rightNode = null;
+	}
+	
+
+}
