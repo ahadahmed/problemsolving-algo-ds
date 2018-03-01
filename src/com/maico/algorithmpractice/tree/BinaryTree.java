456bf8224cf9983d4e933c7478f40495bcb2875a
ahad <ahad.ahmed@surecash.net>
2018-03-01 15:08:20 +0600

    implement bredth first search.

---
+++ src/com/maico/algorithmpractice/tree/BinaryTree.java
@@ -0,0 +1,56 @@
+package com.maico.algorithmpractice.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+	
+	Node root;
+	
+	
+	public BinaryTree() {
+		root = null;
+	}
+	
+	public BinaryTree(int key) {
+		root = new Node(key);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BinaryTree binaryTree = new BinaryTree();
+		binaryTree.root = new Node(8);
+		binaryTree.root.setLeftNode(new Node(5));
+		binaryTree.root.setRightNode(new Node(4));
+		
+		binaryTree.root.getLeftNode().setLeftNode(new Node(9));
+		binaryTree.root.getLeftNode().setRightNode(new Node(7));
+		
+		binaryTree.root.getRightNode().setLeftNode(new Node(11));
+		binaryTree.root.getRightNode().setRightNode(new Node(7));
+		
+		bfs(binaryTree.root);
+
+	}
+	
+	
+	public static void bfs(Node root) {
+		
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		
+		while(!queue.isEmpty()) {
+			Node currentNode = queue.poll();
+			System.out.print(currentNode.getNodeValue() + " ");
+			if(currentNode.getLeftNode() != null) {
+				queue.add(currentNode.getLeftNode());
+			}
+			
+			if(currentNode.getRightNode() != null) {
+				queue.add(currentNode.getRightNode());
+			}
+		}
+	}
+
+}
