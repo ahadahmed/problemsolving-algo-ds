234c50dbe27023398761b0b4522093f6563f9dd9
ahad <ahad.ahmed@surecash.net>
2018-03-11 10:35:13 +0600

    Writing a large file for FreightOS problem(HyperLogLog).

--- src/com/maico/f8/DistinctWordCountMain.java
+++ src/com/maico/f8/DistinctWordCountMain.java
@@ -5,10 +5,7 @@ public class DistinctWordCountMain {
 	
 	public static void main(String[] args) {
 		LargerFileWriter largeFileWriter = new LargerFileWriter();
-		ReadingLargeFile largeFileReader = new ReadingLargeFile();
-//		largeFileWriter.writeToFile();
-		largeFileReader.readingFileLineByLine();
-		
+		largeFileWriter.writeToFile();
 	}
 
 }
