52f68831bc3ce621b71cd3aa26b6c9448091d76b
ahad <ahad@surecash.net>
2019-06-30 17:37:46 +0600

    Array left rotation by number of elements.

--- src/com/maico/datastructure/array/Rotation.java
+++ src/com/maico/datastructure/array/Rotation.java
@@ -12,26 +12,23 @@ public class Rotation {
 
 
     /**
-     * rotating an array by 'd' elements using a temporary array
-     * @param originalArray
-     * @param numberOfElementsTobeRotated
-     * @return
+     * array rotation using a temporary array
      */
-    public int[] usingTempArray(int[] originalArray, int numberOfElementsTobeRotated){
+    public int[] usingTempArray(int[] originalArray, int rotationByNumber){
         int[] rotatedArray = new int[originalArray.length];
-        int[] tempArray = new int[numberOfElementsTobeRotated];
-        for (int j = 0; j < numberOfElementsTobeRotated; j++ ){
+        int[] tempArray = new int[rotationByNumber];
+        for (int j = 0; j < rotationByNumber; j++ ){
             tempArray[j] = originalArray[j];
         }
         System.out.println(Arrays.toString(tempArray));
 
-        for( int j=numberOfElementsTobeRotated;  j < originalArray.length; j++){
-            rotatedArray[j - numberOfElementsTobeRotated] = originalArray[j];
+        for( int j=rotationByNumber;  j < originalArray.length; j++){
+            rotatedArray[j - rotationByNumber] = originalArray[j];
         }
         System.out.println(Arrays.toString(rotatedArray));
 
         for(int i= 0; i<tempArray.length; i++){
-            rotatedArray[rotatedArray.length - (numberOfElementsTobeRotated-i)] = tempArray[i];
+            rotatedArray[rotatedArray.length - (rotationByNumber-i)] = tempArray[i];
         }
         System.out.println(Arrays.toString(rotatedArray));
 
@@ -39,69 +36,13 @@ public class Rotation {
     }
 
 
-    /**
-     * method for rotating an array by 'd' elements using reverse algorithm
-     * @param originalArray
-     * @param numberOfElementsTobeRotated
-     * @return
-     */
+    public void leftRotateByOne(int[] arr){
 
-    public int[] usingReverseAlgorithm(int[] originalArray, int numberOfElementsTobeRotated){
-        reverseArray(originalArray, 0, numberOfElementsTobeRotated -1);
-        reverseArray(originalArray, numberOfElementsTobeRotated, originalArray.length -1);
-        reverseArray(originalArray, 0, originalArray.length -1);
-        return originalArray;
-    }
-
-
-
-
-    /**
-     * method for reversing an array
-     * @param array
-     * @param startIndex
-     * @param endIndex
-     */
-
-
-    private void reverseArray(int[] array, int startIndex, int endIndex){
-
-        while(startIndex < endIndex){
-            int temp = array[startIndex];
-            array[startIndex] = array[endIndex];
-            array[endIndex] = temp;
-            startIndex++;
-            endIndex--;
-        }
-
-    }
-
-
-    /**
-     * Method for rotating array in cyclic order by one (clockwise)
-     * @param originalArray
-     * @return
-     */
-
-    public int[] cyclicRotation(int[] originalArray){
-        int temp = originalArray[originalArray.length - 1];
-
-        for(int i = originalArray.length -2; i >=0 ; i--){
-            originalArray[i+1] = originalArray[i];
-        }
-        originalArray[0] = temp;
-
-
-        return originalArray;
     }
 
     public static void main(String[] args) {
         Rotation arrayRotation = new Rotation();
-//        arrayRotation.usingTempArray(arrayRotation.originalArray, 2);
-        System.out.println("Array before rotation -- " + Arrays.toString(arrayRotation.originalArray));
-//        arrayRotation.usingReverseAlgorithm(arrayRotation.originalArray, 3);
-        arrayRotation.cyclicRotation(arrayRotation.originalArray);
-        System.out.println("Array after rotaion -- "+Arrays.toString(arrayRotation.originalArray));
+        arrayRotation.usingTempArray(arrayRotation.originalArray, 2);
     }
 
 }
