0d1209c3c104bd5d0df4e7c025f990205b40aee0
ahadahmed-home <jpmhosting@gmail.com>
2019-10-11 20:17:45 +0600

    Implemenation of vertical order traversal using BFS.

--- src/com/maico/datastructure/tree/PrintBinaryTreeInVerticalOrder.java
+++ src/com/maico/datastructure/tree/PrintBinaryTreeInVerticalOrder.java
@@ -1,6 +1,5 @@
 package com.maico.datastructure.tree;
 
-import java.io.OutputStream;
 import java.util.*;
 
 public class PrintBinaryTreeInVerticalOrder {
@@ -15,7 +14,8 @@ public class PrintBinaryTreeInVerticalOrder {
         while (numberOfNodes-- > 0){
             root = BinaryTree.insertNode(root,scanner.nextInt());
         }
-        verticalOrderTraversal(root, 0);
+//        verticalOrderTraversal(root, 0);
+        verticalTraversalUsingBFS(root);
         for(Map.Entry<Integer, List<Node>> entry: horizontalDistanceMap.entrySet()){
             System.out.print(entry.getKey()+"--> ");
             for(Node n : entry.getValue()){
@@ -49,4 +49,41 @@ public class PrintBinaryTreeInVerticalOrder {
 
     }
 
+
+
+    private static void verticalTraversalUsingBFS(Node root){
+         class Pair{
+             Node n;
+             Integer hd;
+            Pair(Node n, Integer i){
+                this.n = n;
+                this.hd =i;
+            }
+        }
+        Queue<Pair> queue = new LinkedList<>();
+        Integer horizontalDistance = 0;
+
+        queue.add(new Pair(root, horizontalDistance));
+
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+            horizontalDistance = pair.hd;
+            if(horizontalDistanceMap.get(pair.hd) == null){
+                List<Node> nodes = new ArrayList<>();
+                nodes.add(pair.n);
+                horizontalDistanceMap.put(pair.hd, nodes);
+            }else {
+                horizontalDistanceMap.get(pair.hd).add(pair.n);
+            }
+
+            if(pair.n.getLeftNode() != null){
+                queue.add(new Pair(pair.n.getLeftNode(), horizontalDistance - 1));
+            }
+
+            if(pair.n.getRightNode() != null){
+                queue.add(new Pair(pair.n.getRightNode(), horizontalDistance + 1));
+            }
+
+        }
+    }
 }
