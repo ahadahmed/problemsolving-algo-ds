db312e0a5fc73118eb3e91105e70dd9f3fa4167b
ahadahmed-home <jpmhosting@gmail.com>
2019-10-11 14:38:11 +0600

    Move Tree creation logic into BinaryTree class Implemenation of vertical order traversal.

--- src/com/maico/datastructure/tree/PrintBinaryTreeInVerticalOrder.java
+++ src/com/maico/datastructure/tree/PrintBinaryTreeInVerticalOrder.java
@@ -1,5 +1,6 @@
 package com.maico.datastructure.tree;
 
+import java.io.OutputStream;
 import java.util.*;
 
 public class PrintBinaryTreeInVerticalOrder {
@@ -8,30 +9,20 @@ public class PrintBinaryTreeInVerticalOrder {
     private static TreeMap<Integer, List<Node>> horizontalDistanceMap = new TreeMap<>();
 
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int numberOfNodes = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int numberOfNodes = scanner.nextInt();
 
-//        int[] nodes = {20,8,22,4,12,21,24,10,14,23,25};
-        int[] nodes = {2, 1, 4, 3, 5, 6, 7};
-
-        for(int i = 0; i < nodes.length; i++){
-            root = BinaryTree.insertNode(root,nodes[i]);
-        }
-
-        /*while (numberOfNodes-- > 0){
+        while (numberOfNodes-- > 0){
             root = BinaryTree.insertNode(root,scanner.nextInt());
-        }*/
-//        verticalOrderTraversal(root, 0);
-       /* verticalTraversalUsingBFS(root);
+        }
+        verticalOrderTraversal(root, 0);
         for(Map.Entry<Integer, List<Node>> entry: horizontalDistanceMap.entrySet()){
             System.out.print(entry.getKey()+"--> ");
             for(Node n : entry.getValue()){
                 System.out.print(n.getNodeValue()+" ");
             }
             System.out.println();
-        }*/
-
-        System.out.println(checkBinarySearchTree(root));
+        }
     }
 
 
@@ -58,112 +49,4 @@ public class PrintBinaryTreeInVerticalOrder {
 
     }
 
-
-
-    private static void verticalTraversalUsingBFS(Node root){
-         class Pair{
-             Node n;
-             Integer hd;
-            Pair(Node n, Integer i){
-                this.n = n;
-                this.hd =i;
-            }
-        }
-        Queue<Pair> queue = new LinkedList<>();
-        Integer horizontalDistance = 0;
-
-        queue.add(new Pair(root, horizontalDistance));
-
-        while (!queue.isEmpty()) {
-            Pair pair = queue.poll();
-            horizontalDistance = pair.hd;
-            if(horizontalDistanceMap.get(pair.hd) == null){
-                List<Node> nodes = new ArrayList<>();
-                nodes.add(pair.n);
-                horizontalDistanceMap.put(pair.hd, nodes);
-            }else {
-                horizontalDistanceMap.get(pair.hd).add(pair.n);
-            }
-
-            if(pair.n.getLeftNode() != null){
-                queue.add(new Pair(pair.n.getLeftNode(), horizontalDistance - 1));
-            }
-
-            if(pair.n.getRightNode() != null){
-                queue.add(new Pair(pair.n.getRightNode(), horizontalDistance + 1));
-            }
-
-        }
-    }
-
-
-    private static boolean checkBinarySearchTree(Node root) {
-
-        Queue<Node> nodeQueue = new LinkedList<>();
-
-
-        Node leftChild = null;
-        Node rightChild = null;
-
-        if(root.leftNode != null){
-            leftChild = root.leftNode;
-            if(root.nodeValue <= leftChild.nodeValue){
-                return false;
-            }
-        }
-
-        if(root.rightNode != null){
-            rightChild = root.rightNode;
-            if(root.nodeValue >= rightChild.nodeValue){
-                return false;
-            }
-        }
-
-        nodeQueue.add(leftChild);
-        while (!nodeQueue.isEmpty()) {
-            Node temp = nodeQueue.poll();
-            if (root.nodeValue < temp.nodeValue) {
-                return false;
-            } else {
-                if (temp.leftNode != null) {
-
-                    if (temp.nodeValue <= temp.leftNode.nodeValue) {
-                        return false;
-                    }
-                    nodeQueue.add(temp.leftNode);
-                }
-                if (temp.rightNode != null) {
-                    if(temp.nodeValue >= temp.rightNode.nodeValue || root.nodeValue <= temp.nodeValue){
-                        return false;
-                    }
-                    nodeQueue.add(temp.rightNode);
-                }
-            }
-        }
-
-        nodeQueue.add(rightChild);
-
-        while(!nodeQueue.isEmpty()){
-            Node temp = nodeQueue.poll();
-            if(root.nodeValue >= temp.nodeValue){
-                return false;
-            }else{
-                if (temp.leftNode != null) {
-                    if(temp.nodeValue <= temp.leftNode.nodeValue){
-                        return false;
-                    }
-                    nodeQueue.add(temp.leftNode);
-                }
-                if (temp.rightNode != null) {
-                    if(temp.nodeValue >= temp.rightNode.nodeValue){
-                        return false;
-                    }
-                    nodeQueue.add(temp.rightNode);
-                }
-            }
-        }
-
-        return true;
-
-    }
 }
