eb8a64b3873a110108183c4611265c002fe907ad
ahadahmed <ahadahmed.career@gmail.com>
2021-02-07 13:41:14 +0600

    Implemented parallel unit testing for BalancedBrackets

--- src/main/java/com/maico/stack/BalancedBrackets.java
+++ src/main/java/com/maico/stack/BalancedBrackets.java
@@ -9,13 +9,12 @@ import java.util.*;
 public class BalancedBrackets {
 
     public boolean isBalanced(String input){
+        System.out.println(System.currentTimeMillis() +":" +Thread.currentThread().getName() + "--"+ Thread.currentThread().getId());
         Map<Character, Character> matchingBrackets = new HashMap<>();
         matchingBrackets.put(')', '(');
         matchingBrackets.put('}', '{');
         matchingBrackets.put(']', '[');
         Deque<Character> stack = new ArrayDeque<>();
-
-
         for(int i=0; i<input.length(); i++){
             Character letter = input.charAt(i);
             if( matchingBrackets.containsValue(letter)){
@@ -30,43 +29,7 @@ public class BalancedBrackets {
                 }
             }
         }
-
-
         return stack.size() == 0;
     }
-
-   public boolean balancedBracket(String str){
-
-        String openingbrackets = "({[";
-        String closingbrackets = ")}]";
-
-       Map<Character, Character> matchingBrackets = new HashMap<>();
-       matchingBrackets.put(')', '(');
-       matchingBrackets.put('}', '{');
-       matchingBrackets.put(']', '[');
-       List<Character> stack = new ArrayList<>();
-
-       for(int i=0; i<str.length(); i++){
-           char letter = str.charAt(i);
-           if( openingbrackets.indexOf(letter) != -1){
-               stack.add(letter);
-           } else if (closingbrackets.indexOf(letter) != -1) {
-
-               if (stack.size() == 0) {
-                   return false;
-               }
-
-               if (stack.get(stack.size() - 1) == matchingBrackets.get(letter)) {
-                   stack.remove(stack.size() -1);
-               }else {
-                   return false;
-               }
-
-           }
-       }
-
-
-        return stack.size() == 0;
-   }
 
 }
