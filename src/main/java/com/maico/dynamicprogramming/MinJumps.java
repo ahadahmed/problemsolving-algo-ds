f9843e6bfec091c6c446f180a460c8b814d268e4
ahadahmed <ahadahmed.career@gmail.com>
2021-03-17 09:57:11 +0600

    Implemented MinJumps problem  along with junit test cases.

---
+++ src/main/java/com/maico/dynamicprogramming/MinJumps.java
@@ -0,0 +1,27 @@
+package com.maico.dynamicprogramming;
+
+/**
+ * @author ahadahmed
+ * @see <a href="https://www.algoexpert.io/questions/Min%20Number%20Of%20Jumps"> see this</a>
+ */
+public class MinJumps {
+
+    public int minNumberOfJumps(int[] array){
+        if(array.length == 1){
+            return 0;
+        }
+        int maxReach = array[0];
+        int steps = array[0];
+        int minJumps = 0;
+        for(int i=1; i < array.length - 1; i++){
+            maxReach = Math.max(maxReach, i + array[i]);
+            steps--;
+            if(steps == 0){
+                minJumps++;
+                steps = maxReach - i;
+            }
+        }
+        System.out.println(minJumps);
+        return minJumps + 1;
+    }
+}
