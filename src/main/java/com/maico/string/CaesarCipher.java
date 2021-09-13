3636a370c5f83576142438008add3725d0fa85a7
Ahad Ahmed <ahadahmed.career@gmail.com>
2021-09-13 12:35:25 +0600

    Fix issue while the key value is greater than 26, having modulus of key by 26.

--- src/main/java/com/maico/string/CaesarCipher.java
+++ src/main/java/com/maico/string/CaesarCipher.java
@@ -5,6 +5,7 @@ public class CaesarCipher {
     public String caesarCipherEncryptor(String input, int key){
 
         char[] chars = new char[input.length()];
+        key = key % 26;
         for(int i=0; i < input.length(); i++){
             int c = input.charAt(i);
             int nlc = c + key;
